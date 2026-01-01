86057e18a43becbe98766c3eccc7b093a682ec3e
frz-reeyep <alfarizkian.muhammad@gmail.com>
2026-01-01 16:41:26 +0700

    Add feature to change name directly

--- commands/set-name.js
+++ commands/set-name.js
@@ -3,7 +3,7 @@ const {bot} = require("../bot.js")
 const {inspect} = require("util")
 
 module.exports = {
-    response: async ({message}, next) => {
+    response: async ({message, captures}, next) => {
         const path = "./data/profiles/" + (message.toBaileys().key?.participantAlt || message.toBaileys().key?.remoteJidAlt) + ".json"
         let data
         if(!fs.existsSync(path)) {
@@ -16,13 +16,33 @@ module.exports = {
         } else {
             data = JSON.parse(await fs.readFileSync(path, "utf8"))
         }
-        await message.reply("Send a text to change name!")
-        let msg = await bot.waitForMessage((m) => m.sender.id === message.sender.id && m.type === "text", 60*10*10*10)
-        if(!msg) return "Time to change your name is over"
-        else if (msg.text.includes("@")) return "You can't add @ in your"
-        data.name = msg.text
-        await fs.writeFileSync(path, JSON.stringify(data, null, 2))
-        return "Successfully set your name to "+msg
+        if(captures.paramString) {
+            data.name = captures.paramString
+            if(data.name.length >= 50) return "The name must be less then 50 caracters"
+            else if (data.name.includes("@")) return "You can't add @ in your"
+            await fs.writeFileSync(path, JSON.stringify(data, null, 2))
+            return "Successfully set your name to "+data.name
+        } else {
+            await message.reply("Send a text to change name!")
+            let msg = await bot.waitForMessage((m) => m.sender.id === message.sender.id && m.type === "text", 60*10*10*10)
+            if(!msg) return "Time to change your name is over"
+            else if (msg.text.toLowerCase() == "@cancel") {
+                await msg.reply("Change name is canceled")
+                return
+            }
+            else if (msg.text.includes("@")) {
+                await msg.reply("You can't add @ in your")
+                return
+            }
+            else if(msg.text.length >= 50) {
+                await msg.reply("The name must be less then 50 caracters")
+                return
+            }
+            data.name = msg.text
+            await fs.writeFileSync(path, JSON.stringify(data, null, 2))
+            await msg.reply("Successfully set your name to "+data.name)
+            return
+        }
     },
     options: {
         description: "set your name",
